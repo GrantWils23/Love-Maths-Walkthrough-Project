38faa9d31c263ba7ffeefac59980d30b2a87d1d1
Grant <GrantWils.23@googlemail.com>
2021-10-13 19:42:53 +0000

    add check answer function and calculateCorrectAnswer functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for(let button of buttons){
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,12 +37,40 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Check the answer against the first element in the
+ * returned calculatedCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value); //need to use value here as it is an input field and NOT use innerText//
+    let calculatedAnswer = calculateCorrectAnswer();        // call to calculatedCorrectAnswer function to get the correct answer.
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwwww.... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);   // last thing to do after completing the check is to run the same game type again for the next question
 }
 
-function calculateCorrectAnswer() {
 
+/**
+ * Gets the operands (the numbers) amd the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer
+ */
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];  // the return from this function is an array, (index0 = answer), (index1 = the type of operation)
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
